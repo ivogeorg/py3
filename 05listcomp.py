6e413ef945db8dc779c4c2100b274a43ade56bb4
Ivo Georgiev <ivogeorg@gmail.com>
2016-11-01 17:05:42 -0600

    Section 5 list comps

--- 05listcomp.py
+++ 05listcomp.py
@@ -19,3 +19,12 @@ for x in [1,2,3]:
             combs.append((x, y))
 print(combs)
 
+# 2.
+# flatten a list with two 'for'
+vec = [[1,2,3], [4,5,6], [7,8,9]]
+print([num for elem in vec for num in elem])
+
+# unravel left to right (outer to inner)
+vec = [[[1,2,3], [4,5,6], [7,8,9]], [[1,2,3], [4,5,6], [7,8,9]]]
+print([num for outer in vec for inner in outer for num in inner])
+
